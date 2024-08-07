1829403eb04968dcf4fa74077721b7bc6cbbfe47
raheel-muhammad <raheelmuhammadpervaiz@gmail.com>
2024-08-07 17:47:38 +0500

    changes has been completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,7 @@ import { motion } from "framer-motion"
 
 const container = (delay) => ({
     hidden: { x: -100, opacity: 0 },
-    visible: { x: 0, opacity: 1 },
-    transition: { duration: 0.5, delay: delay }
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
 })
 
 const Hero = () => {
